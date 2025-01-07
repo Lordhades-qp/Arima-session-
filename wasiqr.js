a0c53ce2976e37e71da22c6091f3e5e6204599b5
LORD HADÈS ©T☢️ <hadeslord087@gmail.com>
2025-01-07 12:52:18 +0000

    Update wasiqr.js

--- wasiqr.js
+++ wasiqr.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 let router = express.Router()
 const pino = require("pino");
 const {
-	default: Wasi_Tech,
+	default: Arima_Tech,
 	useMultiFileAuthState,
 	jidNormalizedUser,
 	Browsers,
@@ -28,13 +28,13 @@ const {
 } = require("node:fs/promises")
 router.get('/', async (req, res) => {
 	const id = makeid();
-	async function WASI_MD_QR_CODE() {
+	async function ARIMA_BOT_QR_CODE() {
 		const {
 			state,
 			saveCreds
 		} = await useMultiFileAuthState('./temp/' + id)
 		try {
-			let Qr_Code_By_Wasi_Tech = Wasi_Tech({
+			let Qr_Code_By_Arima_Tech = Arima_Tech({
 				auth: state,
 				printQRInTerminal: false,
 				logger: pino({
@@ -43,8 +43,8 @@ router.get('/', async (req, res) => {
 				browser: Browsers.macOS("Desktop"),
 			});
 
-			Qr_Code_By_Wasi_Tech.ev.on('creds.update', saveCreds)
-			Qr_Code_By_Wasi_Tech.ev.on("connection.update", async (s) => {
+			Qr_Code_By_Arima_Tech.ev.on('creds.update', saveCreds)
+			Qr_Code_By_Arima_Tech.ev.on("connection.update", async (s) => {
 				const {
 					connection,
 					lastDisconnect,
@@ -56,38 +56,37 @@ router.get('/', async (req, res) => {
 					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
 					await delay(800);
 				   let b64data = Buffer.from(data).toString('base64');
-				   let session = await Qr_Code_By_Wasi_Tech.sendMessage(Qr_Code_By_Wasi_Tech.user.id, { text: '' + b64data });
+				   let session = await Qr_Code_By_Wasi_Tech.sendMessage(Qr_Code_By_Arima_Tech.user.id, { text: '' + b64data });
 	
-				   let WASI_MD_TEXT = `
-*_Session Connected By FEARLESS*
-*_Made With 🤍_*
+				   let ARIMA_BOT_TEXT = `
+*_Session Connected By ARIMA*
+*_Made With 🌹_*
 ______________________________________
 ╔════◇
-║ *『 AMAZING YOU'VE CHOSEN ZENITSU CRASH V2 』*
-║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
+║ *『 VOUS AVEZ CHOISI ARIMA BOT  』*
+║ _Tu as fini la première étape du déploiement de ton bot WhatsApp._
 ╚════════════════════════╝
 ╔═════◇
 ║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║❒ *Ytube:* _https://www.youtube.com/@BTSMODZ
-║❒ *Owner:* https://wa.me/2348075952205_
-║❒ *Repo:* _https://github.com/Fearless-tech1_
-║❒ *WaGroup:* _https://chat.whatsapp.com/C3GFThC0tIpGaJY9DFUeCK
-║❒ *WaChannel:* _https://whatsapp.com/channel/0029VahusSh0QeaoFzHJCk2x
-║❒ *Plugins:* _https://github.com/Fearless-tech1 
+║❒ *Owner:* https://wa.me/2250565647864_
+║❒ *Repo:* _github.com/Lordhades-qp/ARIMA-BOT_
+║❒ *WaGroup:* _https://chat.whatsapp.com/LcD3Ei4yWuCK6DXSBcDXLk
+║❒ *WaChannel:* _https://whatsapp.com/channel/0029VatUVBSHrDZcV6K0DH0I
+║❒ *Plugins:* _github.com/Lordhades-qp/ARIMA-BOT 
 ╚════════════════════════╝
 _____________________________________
 	
-_Don't Forget To Give Star To My Repo_`
-	 await Qr_Code_By_Wasi_Tech.sendMessage(Qr_Code_By_Wasi_Tech.user.id,{text:WASI_MD_TEXT},{quoted:session})
+_n'oublie pas de donner une étoile 🌟 à mon repos_`
+	 await Qr_Code_By_Arima_Tech.sendMessage(Qr_Code_By_Arima_Tech.user.id,{text:ARIMA_BOT_TEXT},{quoted:session})
 
 
 
 					await delay(100);
-					await Qr_Code_By_Wasi_Tech.ws.close();
+					await Qr_Code_By_Arima_Tech.ws.close();
 					return await removeFile("temp/" + id);
 				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
 					await delay(10000);
-					WASI_MD_QR_CODE();
+					ARIMA_MD_QR_CODE();
 				}
 			});
 		} catch (err) {
@@ -100,6 +99,6 @@ _Don't Forget To Give Star To My Repo_`
 			await removeFile("temp/" + id);
 		}
 	}
-	return await WASI_MD_QR_CODE()
+	return await ARIMA_MD_QR_CODE()
 });
 module.exports = router
